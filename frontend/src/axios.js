988147f5beb55da3e90228293f598ccd26e602e0
Jesus <1104xyz@gmail.com>
2021-06-26 19:32:34 -0400

    aumentando timeout de axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
     baseURL: SERVER,
-    timeout: 2000,
+    timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     }
