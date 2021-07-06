e408c78bf056b7750524022dc5a30cd04dcb3b2a
Jesus Pacheco <1104xyz@gmail.com>
2021-07-06 08:58:52 -0400

    modificando la instance axios

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 const ax = axios.create({
     baseURL: SERVER,
     timeout: 30000,
-    headers: {
-        'Content-Type': 'application/json',
-    }
 })
 
 export default ax
