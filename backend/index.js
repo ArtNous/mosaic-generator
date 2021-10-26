1be0a65a91a6e32d17ff26dac0180f079da088e5
Jesus Pacheco <1104xyz@gmail.com>
2021-10-26 10:51:31 -0400

    logging images requests

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ const process = require('process')
 const cors = require('cors')
 const createRoutes = require('./routes')
 
+// Middleware for console log the image requests
+app.use(function (req, res, next) {
+    console.log(`${req.method} ${req.url}`)
+    next()
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('public'))
